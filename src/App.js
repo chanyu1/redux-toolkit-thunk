9b3b9d89d46bc89754a85fa739cfeb7b34072270
chanyu1 <haneul1ij@gmail.com>
2023-05-29 19:32:02 +0900

    feat: redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import './App.css';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import store from './store';
+import { up } from './counterSlice';
 
-function App() {
-  return <div className="App">test2</div>;
+function Counter() {
+  const dispatch = useDispatch();
+
+  const count = useSelector((state) => {
+    return state.counter.value;
+  });
+
+  const status = useSelector((state) => {
+    return state.counter.status;
+  });
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(up(2));
+        }}
+      >
+        +
+      </button>
+      <div>
+        {count} | {status}
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
